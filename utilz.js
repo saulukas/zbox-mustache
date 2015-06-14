e3326a8e548098051ed3b3a55096f1cac6bdbf2a
saga <saulukas@gmail.com>
2015-06-14 19:28:37 +0300

    some signs of module

--- utilz.js
+++ utilz.js
@@ -1,7 +1,13 @@
+(function (mb, Mustache, undefined) {
+    mb.renderTo = renderTo;
 
+    function renderTo(targetId, templateId, context) {
+        var template = $('#' + templateId).html();
+        var rendered = Mustache.render(template, context);
+        $('#' + targetId).html(rendered);
+    }
 
-function renderTo(targetId, templateId, context) {
-    var template = $('#' + templateId).html();
-    var rendered = Mustache.render(template, context);
-    $('#' + targetId).html(rendered);
-}
+})(window.mb = window.mb || {}, Mustache);
+
+//var brick1 = bake('Mykolas');
+//mb.render(brick1);
